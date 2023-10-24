1a8e1dc3b4175ddceccebb712284864ffd1be623
eppo-r <rceppolito@gmail.com>
2023-10-24 15:19:13 -0400

    merge master main

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# adfast
+
+# Adfast
+
+Press b to skip Youtube ads. Also useful for in-vdeo sponsored content; pressing b toggles 10x speed to get through this quickly. 
+
+To install:
+Follow the instructions here:
+https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked
+
---
+++ content.js
@@ -0,0 +1,43 @@
+let speedToggled = false;
+let intervalID;
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "b") {
+    const video = document.querySelector("video");
+    console.log("Initial video:", video);
+    if (!video) return;
+
+    // Toggle speed
+    video.playbackRate = speedToggled ? 1.0 : 10;
+    speedToggled = !speedToggled;
+    console.log("Speed toggled to:", video.playbackRate);
+
+    // Clear previous interval, if any
+    clearInterval(intervalID);
+
+    // Start new interval to poll for "Skip Ad" button
+    intervalID = setInterval(() => {
+      const skipButton = document.querySelector(".ytp-ad-skip-button");
+      console.log("Skip button:", skipButton);
+
+      if (skipButton) {
+        skipButton.click();
+
+        // Reset speed and rewind video after a delay
+        setTimeout(() => {
+          const newVideo = document.querySelector("video");
+          console.log("New video:", newVideo);
+
+          if (newVideo) {
+            newVideo.playbackRate = 1.0;
+            newVideo.currentTime -= 1; // Rewind the video by 100ms*10x = 1s
+            speedToggled = false;
+            console.log("Speed reset to:", newVideo.playbackRate, "and time reset to:", newVideo.currentTime);
+          }
+        }, 100);
+
+        clearInterval(intervalID); // Stop the interval
+      }
+    }, 10);
+  }
+});
